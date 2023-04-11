e4c5f6cb35160b8eaade020af3af2304cd0022fa
wangtz-wm21 <129672938+wangtz-wm21@users.noreply.github.com>
2023-04-11 15:11:56 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.czprvzv1bj80.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "wangtengzheng-employee"
+customregion = "us-east-1"
